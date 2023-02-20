28c667fdb7a88aa938a4fc0c2ffd5c6fefdfe80c
qsonic <bashkin415@gmail.com>
2023-02-20 19:30:47 +0300

    Поправил кодстайл

--- app/src/main/java/com/example/flashlight/MainActivity.kt
+++ app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -1,45 +1,53 @@
 package com.example.flashlight
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import com.example.flashlight.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var fls: Boolean = false
+    private var isFlashlightActive: Boolean = false
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        flashlight()
-    }
 
-    @SuppressLint("UseCompatLoadingForDrawables")
-    private fun flashlight() {
         val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        val cameraId = cameraManager.cameraIdList[0]
+        val cameraWithFlashlight = cameraManager.cameraIdList.find {
+            cameraManager.getCameraCharacteristics(it)
+                .get(CameraCharacteristics.FLASH_INFO_AVAILABLE) == true
+        }
+
         binding.flashlightButton.setOnClickListener {
-            if (!fls) {
-                try {
-                    binding.flashlightButton.setImageDrawable(getDrawable(R.drawable.baseline_flashlight_on_24))
-                    cameraManager.setTorchMode(cameraId, true)
-                    fls = true
-                } catch (_: CameraAccessException) {
-                }
+            if (cameraWithFlashlight != null) {
+                onFlashlightClicked(cameraManager, cameraWithFlashlight)
             } else {
-                try {
-                    binding.flashlightButton.setImageDrawable(getDrawable(R.drawable.baseline_flashlight_off_24))
-                    cameraManager.setTorchMode(cameraId, false)
-                    fls = false
-                } catch (_: CameraAccessException) {
-                }
+
             }
         }
+    }
+
+    private fun onFlashlightClicked(
+        cameraManager: CameraManager,
+        cameraId: String
+    ) {
+        if (!isFlashlightActive) {
+            binding.flashlightButton.setImageDrawable(
+                AppCompatResources.getDrawable(this, R.drawable.baseline_flashlight_on_24)
+            )
+            cameraManager.setTorchMode(cameraId, true)
+        } else {
+            binding.flashlightButton.setImageDrawable(
+                AppCompatResources.getDrawable(this, R.drawable.baseline_flashlight_off_24)
+            )
+            cameraManager.setTorchMode(cameraId, false)
+        }
+        isFlashlightActive = !isFlashlightActive
     }
 }
